40d486aeab1334384e4e7930767c0835f7a924af
MeikeBos <meike.bos@planet.nl>
2024-12-20 14:07:38 +0100

    added particle velocity measurement kernel and 2d version of slow manifold MR with different coriolis term

--- simulations/kernels.py
+++ simulations/kernels.py
@@ -113,6 +113,12 @@ def measure_vorticity(particle, fieldset, time):
     dudy = (u_dyp - u_dym) / (2 * fieldset.delta_y)
     particle.vorticity = dvdx - dudy
 
+def measure_fluid_velocity(particle, fieldset, time):
+    uf, vf = fieldset.UV.eval(time, particle.depth, particle.lat,
+                              particle.lon, applyConversion=False)
+    particle.uf = uf
+    particle.vf = vf
+
 def remove_at_bounds(particle, fieldset, time):
     """Kernel for deleting particles if they are out of bounds in the small test run."""
     flag_ = False
@@ -209,7 +215,7 @@ def MRAdvectionEC_2D(particle, fieldset, time):
     DvDt = dvdt + uf * dvdx + vf * dvdy
 
     # coriolis force
-    f = 0  # 2 * fieldset.Omega_earth * math.sin(particle.lat * math.pi/180)
+    f = 0  # 2 *fieldset.Omega_earth * math.sin(particle.lat * math.pi/180)
     ucor = -(vf - particle.vp) * f
     vcor = (uf - particle.up) * f
 
@@ -969,7 +975,7 @@ def MRSMAdvectionRK4_2D(particle, fieldset, time):
     DvDt2 = dvdt2 + uf2 * dvdx2 + vf2 * dvdy2
 
     # coriolis force
-    f2 =  2 * fieldset.Omega_earth*math.sin(lat1 * math.pi / 180)
+    f2 =  2 * fieldset.Omega_earth * math.sin(lat1 * math.pi / 180)
     ucor2 = -vf2 * f2
     vcor2 = uf2 * f2
 
@@ -1012,7 +1018,7 @@ def MRSMAdvectionRK4_2D(particle, fieldset, time):
     DvDt3 = dvdt3 + uf3 * dvdx3 + vf3 * dvdy3
 
     # coriolis force
-    f3 = 2 * fieldset.Omega_earth*math.sin(lat2 * math.pi / 180)
+    f3 = 2 * fieldset.Omega_earth * math.sin(lat2 * math.pi / 180)
     ucor3 = -vf3 * f3
     vcor3 = uf3 * f3
 
@@ -1061,6 +1067,206 @@ def MRSMAdvectionRK4_2D(particle, fieldset, time):
 
     u4 = uf4 + Bterm_tau * (DuDt4 + ucor4)
     v4 = vf4 + Bterm_tau * (DvDt4 + vcor4)
+
+    # RK4 INTEGRATION STEP
+    particle_dlon += (u1 + 2 * u2 + 2 * u3 + u4) * particle.dt / 6.0
+    particle_dlat += (v1 + 2 * v2 + 2 * v3 + v4) * particle.dt / 6.0
+
+
+def MRSMAdvectionRK4_2D_v2(particle, fieldset, time):
+    """
+    Advection of particles using the slow manifold approximation
+    of the Maxey-Riley equation in 2D without Basset history term
+    and Faxen corrections. The equation is numerically integrated
+    using the 4th order runge kutta scheme for an overdamped equation.
+    IN version 2 we use the rescaled coriolis parameter:
+     Omega= 2 (1 - B) / (1 + 2 B) * f *tau which adds aditional terms
+     for the coriolis force which are neglegted in the standard version, 
+      as they are order tau^2 without the rescaling.
+    (ODE order 1)
+
+    dependencies:
+    the particle should contain, tau, the stokes time, B, the buoyancy of the
+    particle. The fieldset should contain Omega_earth, the angular velocity
+    of the earth, and delta_x, delta_y, the step for finite
+    difference method gradients. We appromate the time derivative at t
+    (1rst step rk4) with a forward finite difference and the time derivative
+    at t+delta_t (4th step rk4) with a backward finite difference.
+    """
+    Bterm_tau = (2 * (1. - particle.B) / (1. + 2. * particle.B)) * particle.tau
+
+    norm_deltax = 1.0 / (2.0 * fieldset.delta_x)
+    norm_deltay = 1.0 / (2.0 * fieldset.delta_y)
+    norm_deltat = 1.0 / (1.0 * particle.dt)
+    
+    # RK4 STEP 1
+    # fluid field velocity at location of particle
+    (uf1, vf1) = fieldset.UV[time, particle.depth,
+                             particle.lat, particle.lon]
+
+    # calculate time derivative of fluid field
+    (uf_tp1, vf_tp1) = fieldset.UV[time+particle.dt,
+                                   particle.depth, particle.lat, particle.lon]
+    (uf_tm1, vf_tm1) = fieldset.UV[time, particle.depth,
+                                   particle.lat, particle.lon]
+    dudt1 = (uf_tp1 - uf_tm1) * norm_deltat
+    dvdt1 = (vf_tp1 - vf_tm1) * norm_deltat
+
+    # calculate spatial gradients fluid field
+    (u_dxm1, v_dxm1) = fieldset.UV[time, particle.depth,
+                                particle.lat, particle.lon-fieldset.delta_x]
+    (u_dxp1, v_dxp1) = fieldset.UV[time, particle.depth,
+                                particle.lat, particle.lon+fieldset.delta_x]
+    (u_dym1, v_dym1) = fieldset.UV[time, particle.depth,
+                                particle.lat-fieldset.delta_y, particle.lon]
+    (u_dyp1, v_dyp1) = fieldset.UV[time, particle.depth,
+                                particle.lat+fieldset.delta_y, particle.lon]
+    dudx1 = (u_dxp1 - u_dxm1) * norm_deltax
+    dudy1 = (u_dyp1 - u_dym1) * norm_deltay
+    dvdx1 = (v_dxp1 - v_dxm1) * norm_deltax
+    dvdy1 = (v_dyp1 - v_dym1) * norm_deltay
+
+    # caluclate material derivative fluid
+    DuDt1 = dudt1 + uf1 * dudx1 + vf1 * dudy1
+    DvDt1 = dvdt1 + uf1 * dvdx1 + vf1 * dvdy1
+
+    # coriolis force
+    f1 =  2 * fieldset.Omega_earth * math.sin(particle.lat * math.pi / 180)
+    ucor1 = f1 * (-vf1)# - particle.tau * (-DvDt1 + uf1 * dudy1 - vf1 * dudx1))
+    vcor1 = f1 * (uf1)# - particle.tau * (DuDt1 + uf1 * dvdy1 - vf1 * dvdx1))
+
+    u1 = uf1 + Bterm_tau * (DuDt1) - ucor1
+    v1 = vf1 + Bterm_tau * (DvDt1) - vcor1
+    
+    # lon, lat for next step
+    lon1 = particle.lon + 0.5 * u1 * particle.dt
+    lat1 = particle.lat + 0.5 * v1 * particle.dt
+    time1 = time + 0.5 * particle.dt
+    # RK4 STEP 2
+    # fluid field velocity at location of particle
+    (uf2, vf2) = fieldset.UV[time1, particle.depth, lat1, lon1]
+
+    # calculate time derivative of fluid field
+    (uf_tp2, vf_tp2) = fieldset.UV[time+particle.dt,
+                                   particle.depth, lat1, lon1]
+    (uf_tm2, vf_tm2) = fieldset.UV[time,
+                                   particle.depth, lat1, lon1]
+    dudt2 = (uf_tp2 - uf_tm2) * norm_deltat
+    dvdt2 = (vf_tp2 - vf_tm2) * norm_deltat
+
+    # calculate spatial gradients fluid field
+
+    (u_dxm2, v_dxm2) = fieldset.UV[time1, particle.depth,
+                                lat1, lon1 - fieldset.delta_x]
+    (u_dxp2, v_dxp2) = fieldset.UV[time1, particle.depth,
+                                lat1, lon1 + fieldset.delta_x]
+    (u_dym2, v_dym2) = fieldset.UV[time1, particle.depth,
+                                lat1 - fieldset.delta_y, lon1]
+    (u_dyp2, v_dyp2) = fieldset.UV[time1, particle.depth,
+                                lat1 + fieldset.delta_y, lon1]
+    dudx2 = (u_dxp2 - u_dxm2) * norm_deltax
+    dudy2 = (u_dyp2 - u_dym2) * norm_deltay
+    dvdx2 = (v_dxp2 - v_dxm2) * norm_deltax
+    dvdy2 = (v_dyp2 - v_dym2) * norm_deltay
+
+    # caluclate material derivative fluid
+    DuDt2 = dudt2 + uf2 * dudx2 + vf2 * dudy2
+    DvDt2 = dvdt2 + uf2 * dvdx2 + vf2 * dvdy2
+
+    # coriolis force
+    f2 =  2 * fieldset.Omega_earth * math.sin(lat1 * math.pi / 180)
+    ucor2 = f2 * (-vf2) # - particle.tau * (-DvDt2 + uf2 * dudy2 - vf2 * dudx2))
+    vcor2 = f2 * (uf2) # - particle.tau * (DuDt2 + uf2 * dvdy2 - vf2 * dvdx2))
+
+    u2 = uf2 + Bterm_tau * (DuDt2) - ucor2
+    v2 = vf2 + Bterm_tau * (DvDt2) - vcor2
+
+    # lon, lat for next step
+    lon2 = particle.lon + 0.5 * u2 * particle.dt
+    lat2 = particle.lat + 0.5 * v2 * particle.dt
+    time2 = time + 0.5 * particle.dt
+
+    # RK4 STEP 3
+    # fluid field velocity at location of particle
+    (uf3, vf3) = fieldset.UV[time2, particle.depth, lat2, lon2]
+
+    # calculate time derivative of fluid field
+    (uf_tp3, vf_tp3) = fieldset.UV[time+particle.dt,
+                                   particle.depth, lat2, lon2]
+    (uf_tm3, vf_tm3) = fieldset.UV[time,
+                                   particle.depth, lat2, lon2]
+    dudt3 = (uf_tp3 - uf_tm3) * norm_deltat
+    dvdt3 = (vf_tp3 - vf_tm3) * norm_deltat
+
+    # calculate spatial gradients fluid field
+    (u_dxm3, v_dxm3) = fieldset.UV[time2, particle.depth,
+                                   lat2, lon2 - fieldset.delta_x]
+    (u_dxp3, v_dxp3) = fieldset.UV[time2, particle.depth,
+                                   lat2, lon2 + fieldset.delta_x]
+    (u_dym3, v_dym3) = fieldset.UV[time2, particle.depth,
+                                   lat2 - fieldset.delta_y, lon2]
+    (u_dyp3, v_dyp3) = fieldset.UV[time2, particle.depth,
+                                   lat2 + fieldset.delta_y, lon2]
+    dudx3 = (u_dxp3 - u_dxm3) * norm_deltax
+    dudy3 = (u_dyp3 - u_dym3) * norm_deltay
+    dvdx3 = (v_dxp3 - v_dxm3) * norm_deltax
+    dvdy3 = (v_dyp3 - v_dym3) * norm_deltay
+
+    # caluclate material derivative fluid
+    DuDt3 = dudt3 + uf3 * dudx3 + vf3 * dudy3
+    DvDt3 = dvdt3 + uf3 * dvdx3 + vf3 * dvdy3
+
+    # coriolis force
+    f3 = 2 * fieldset.Omega_earth * math.sin(lat2 * math.pi / 180)
+    ucor3 = f3 * (-vf3) #- particle.tau * (-DvDt3 + uf3 * dudy3 - vf3 * dudx3))
+    vcor3 = f3 * (uf3) # - particle.tau * (DuDt3 + uf3 * dvdy3 - vf3 * dvdx3))
+
+    u3 = uf3 + Bterm_tau * (DuDt3) - ucor3
+    v3 = vf3 + Bterm_tau * (DvDt3) - vcor3
+
+    # lon, lat for next step
+    lon3 = particle.lon + u3 * particle.dt
+    lat3 = particle.lat + v3 * particle.dt
+    time3 = time + particle.dt
+
+    # RK4 STEP 4
+    # fluid field velocity at location of particle
+    (uf4, vf4) = fieldset.UV[time3, particle.depth, lat3, lon3]
+
+    # calculate time derivative of fluid field
+    (uf_tp4, vf_tp4) = fieldset.UV[time + particle.dt,
+                                   particle.depth, lat3, lon3]
+    (uf_tm4, vf_tm4) = fieldset.UV[time, particle.depth,
+                                   lat3, lon3]
+    dudt4 = (uf_tp4 - uf_tm4) * norm_deltat
+    dvdt4 = (vf_tp4 - vf_tm4) * norm_deltat
+
+    # calculate spatial gradients fluid field
+    (u_dxm4, v_dxm4) = fieldset.UV[time, particle.depth,
+                                   lat3, lon3 - fieldset.delta_x]
+    (u_dxp4, v_dxp4) = fieldset.UV[time, particle.depth,
+                                   lat3, lon3 + fieldset.delta_x]
+    (u_dym4, v_dym4) = fieldset.UV[time, particle.depth,
+                                   lat3 - fieldset.delta_y, lon3]
+    (u_dyp4, v_dyp4) = fieldset.UV[time, particle.depth,
+                                   lat3 + fieldset.delta_y, lon3]
+    dudx4 = (u_dxp4 - u_dxm4) * norm_deltax
+    dudy4 = (u_dyp4 - u_dym4) * norm_deltay
+    dvdx4 = (v_dxp4 - v_dxm4) * norm_deltax
+    dvdy4 = (v_dyp4 - v_dym4) * norm_deltay
+
+    # caluclate material derivative fluid
+    DuDt4 = dudt4 + uf4 * dudx4 + vf4 * dudy4
+    DvDt4 = dvdt4 + uf4 * dvdx4 + vf4 * dvdy4
+
+    # coriolis force
+    f4 = 2 * fieldset.Omega_earth * math.sin(lat3 * math.pi / 180)
+    ucor4 = f4 * (-vf4) # - particle.tau * (-DvDt4 + uf4 * dudy4 - vf4 * dudx4))
+    vcor4 = f4 * (uf4) # - particle.tau * (DuDt4 + uf4 * dvdy4 - vf4 * dvdx4))
+
+
+    u4 = uf4 + Bterm_tau * (DuDt4) - ucor4
+    v4 = vf4 + Bterm_tau * (DvDt4) - vcor4
 
     # RK4 INTEGRATION STEP
     particle_dlon += (u1 + 2 * u2 + 2 * u3 + u4) * particle.dt / 6.0
